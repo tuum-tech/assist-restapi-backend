b3f7a766dca7e4134e5e71ef714a98176a6491fa
Kiran Pachhai <kiran.pachhai@gmail.com>
2020-07-01 10:16:52 -0400

    Fixing prefetch token issue on assist

--- app/__init__.py
+++ app/__init__.py
@@ -48,15 +48,8 @@ connect(
          config.MONGO['HOST'] + ":" + str(config.MONGO['PORT']) + "/?authSource=admin"
 )
 
-# Set up CORS
-cors = CORS(
-    allow_all_origins=True,
-    allow_all_headers=True,
-    allow_all_methods=True)
-# Initialize the application
 LOG.info("Initializing the Falcon REST API service...")
 application = App(middleware=[
-    cors.middleware,
     AuthMiddleware(),
 ])
 
--- app/middleware/auth.py
+++ app/middleware/auth.py
@@ -9,9 +9,10 @@ class AuthMiddleware(object):
     def process_request(self, req, res):
         prefetch_token = req.get_header('ACCESS-CONTROL-REQUEST-METHOD')
         if prefetch_token:
+            res.complete = True
             return True
-
-        token = req.get_header('authorization')
+        
+        token = req.get_header('Authorization')
 
         if token is None:
             description = 'Please provide an auth token as part of the request'
@@ -25,3 +26,26 @@ class AuthMiddleware(object):
         if config.SECRET_KEY != token:
             return False
         return True
+
+    def process_response(self, req, resp, resource, req_succeeded):
+        resp.set_header('Access-Control-Allow-Origin', '*')
+
+        if (req_succeeded
+            and req.method == 'OPTIONS'
+            and req.get_header('Access-Control-Request-Method')
+        ):
+            # NOTE(kgriffs): This is a CORS preflight request. Patch the
+            #   response accordingly.
+            allow = resp.get_header('Allow')
+            resp.delete_header('Allow')
+
+            allow_headers = req.get_header(
+                'Access-Control-Request-Headers',
+                default='*'
+            )
+
+            resp.set_headers((
+                ('Access-Control-Allow-Methods', allow),
+                ('Access-Control-Allow-Headers', allow_headers),
+                ('Access-Control-Max-Age', '86400'),  # 24 hours
+            ))
