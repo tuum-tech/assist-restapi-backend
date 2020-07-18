6bab1140e0799c990ce50a4f2935fe4e14c9967d
Ricardo Trapp <ricardo.trapp@gmail.com>
2020-07-17 22:01:28 -0400

    Service Statistics

--- app/api/v1/servicecount.py
+++ app/api/v1/servicecount.py
@@ -16,10 +16,47 @@ class GetServiceCount(BaseResource):
     """
 
     def on_get(self, req, res, service, did):
-        rows = Servicecount.objects(did=did)
+        LOG.info(f'Enter /v1/service_count/{service}/{did}')
+        rows = Servicecount.objects(did=did.replace("did:elastos:", "").split("#")[0])
         if rows:
             obj = rows[0].service_count_as_dict(service)
-            self.on_success(res, obj)
         else:
-            raise AppError()
+            obj = {
+                "id": "",
+                "did": did,
+                "service": service,
+                "count": 0,
+                "total_count": 0,
+                "created": "never",
+                "modified": "never"
+            }
+        self.on_success(res, obj)
+
+class GetServiceStatistics(BaseResource):
+    """
+    Handle for endpoint: /v1/service_count/statistics
+    """
+
+    def on_get(self, req, res):
+        LOG.info(f'Enter /v1/service_count/statistics')
+        rows = Servicecount.objects()
+
+        obj = {
+            "did_publish":{
+                "users": 0,
+                "today": 0,
+                "total": 0
+            }
+        }
+
+        if rows:
+            for item in rows:
+                row = item.as_dict()
+                if  row["data"]["did_publish"]:
+                    obj["did_publish"]["users"] += 1
+                    obj["did_publish"]["today"] +=  row["data"]["did_publish"]["count"]
+                    obj["did_publish"]["total"] +=  row["data"]["did_publish"]["total_count"]
+
+        
+        self.on_success(res, obj)
 
