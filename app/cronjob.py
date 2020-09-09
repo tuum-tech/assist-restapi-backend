3f729bba16d8f985b061a71bd8335a9b728d7cc7
Kiran Pachhai <kiran.pachhai@gmail.com>
2020-09-09 13:53:14 -0400

    Saving row status to database after cancellation

--- app/cronjob.py
+++ app/cronjob.py
@@ -147,6 +147,9 @@ def cron_send_tx_to_did_sidechain():
                 LOG.info(
                     f"The id '{row.id}' with DID '{row.did}' has been in Pending state for the last hour. Changing its state to Cancelled")
                 row.status = config.SERVICE_STATUS_CANCELLED
+                row.extraInfo = {
+                    "reason": "Was in pending state for more than 1 hour"
+                }
                 row.save()
                 continue
             tx = did_publish.create_raw_transaction(row.did, row.didRequest)
