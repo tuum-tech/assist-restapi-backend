c394e724c43efd0692a5e917203f6216d055055a
Kiran Pachhai <kiran.pachhai@gmail.com>
2021-02-01 19:56:49 -0500

    Fixed an issue of not displaying daily stats correctly

--- app/service/service_stats.py
+++ app/service/service_stats.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime, timedelta
 
-from app.model import Servicecount
+from pymongo import MongoClient
+
+from app import config
 
 
 def get_service_count():
-    rows = Servicecount.objects()
+    mongo_client = MongoClient(config.MONGO_CONNECT_HOST)
+    db = mongo_client.assistdb
 
     result = {
         "did_publish": {}
@@ -16,12 +20,20 @@ def get_service_count():
             "total": 0
         }
 
-    if rows:
-        for item in rows:
-            row = item.as_dict()
-            for service in result.keys():
-                if service in row["data"].keys():
-                    result[service]["users"] += 1
-                    result[service]["today"] += row["data"][service]["count"]
-                    result[service]["total"] += row["data"][service]["total_count"]
+    result_users = db.didtx.aggregate([
+        {"$group": {"_id": "$did", "count": {"$sum": 1}}},
+        {"$project": {"_id": 0, "name": "$_id", "count": "$count"}}
+    ])
+    count_users = 0
+    for _ in result_users:
+        count_users += 1
+    result["did_publish"]["users"] = count_users
+
+    date_today = datetime.utcnow() - timedelta(hours=24)
+    result_today = db.didtx.count_documents({"modified": {"$gt": date_today}})
+    result["did_publish"]["today"] = result_today
+
+    result_total = db.didtx.count_documents({})
+    result["did_publish"]["total"] = result_total
+
     return result
