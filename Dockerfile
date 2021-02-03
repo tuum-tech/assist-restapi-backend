255f55a94ac617a6a298a8684c4c1afe173c1933
Kiran Pachhai <kiran.pachhai@gmail.com>
2021-02-02 20:45:40 -0500

    Added Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.8
+LABEL MAINTAINER="kpachhai"
+
+ADD requirements.txt /src/
+RUN cd /src && pip install -r requirements.txt
+
+ADD app /src/app
+
+WORKDIR /src
+
+EXPOSE 5000
+
+CMD ["gunicorn", "-b", "0.0.0.0:5000", "app:application"]
+
+
--- run.sh
+++ run.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
 
-function start () {
+function start_db () {
     docker container stop tuum-mongo || true && docker container rm -f tuum-mongo || true
     docker run -d --name tuum-mongo                     \
-        -v ${HOME}/.tuum-mongodb-data:/data/db                \
+        -v ${HOME}/.tuum-mongodb-data:/data/db          \
         -e MONGO_INITDB_ROOT_USERNAME=mongoadmin        \
         -e MONGO_INITDB_ROOT_PASSWORD=mongopass         \
         -p 27017:27017                                  \
         mongo
+}
+
+function docker_start () {
+    start_db
+
+    echo "Running using docker..."
+    docker container stop assist-restapi-node || true && docker container rm -f assist-restapi-node || true
+    docker build -t tuumtech/assist-restapi-node .
+    docker run --name assist-restapi-node           \
+      -v ${PWD}/.env:/src/.env              \
+      -p 8000:5000                          \
+      tuumtech/assist-restapi-node
+}
+
+function start () {
+    start_db
 
     virtualenv -p `which python3.7` .venv
     source .venv/bin/activate
@@ -32,6 +48,7 @@ function start () {
 
 function stop () {
     docker container stop tuum-mongo || true && docker container rm -f tuum-mongo || true
+    docker container stop assist-restapi-node || true && docker container rm -f assist-restapi-node || true
     ps -ef | grep gunicorn | awk '{print $2}' | xargs kill -9
 }
 
@@ -39,10 +56,13 @@ case "$1" in
     start)
         start
         ;;
+    docker)
+        docker_start
+        ;;
     stop)
         stop
         ;;
     *)
-    echo "Usage: run.sh {start|stop}"
+    echo "Usage: run.sh {start|docker|stop}"
     exit 1
 esac
