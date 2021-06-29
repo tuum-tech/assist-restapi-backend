2c2431de2b212f46b35dd1879514ef1dfc46daac
Kiran Pachhai <kiran.pachhai@gmail.com>
2021-06-29 12:31:56 -0400

    Removing extra field no longer used

--- app/model/didstate.py
+++ app/model/didstate.py
@@ -6,7 +6,6 @@ from mongoengine import IntField, DateTimeField, Document
 class Didstate(Document):
     currentHeight = IntField()
     currentHeightv2 = IntField(default=0)
-    lastWalletUsed = IntField(default=5)
     created = DateTimeField()
     modified = DateTimeField(default=datetime.datetime.utcnow)
 
@@ -18,7 +17,6 @@ class Didstate(Document):
             "id": str(self.id),
             "currentHeight": self.currentHeight,
             "currentHeightv2": self.currentHeightv2 if self.currentHeightv2 else 1,
-            "lastWalletUsed": self.lastWalletUsed if self.lastWalletUsed else 1,
             "created": str(self.created),
             "modified": str(self.modified)
         }
